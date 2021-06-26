b983ee44384789c333e56332e0555ad706eefcab
AbdullahPS <abdullahtabbal@github.com>
2021-06-26 12:00:20 +0300

    Add loginscreen with animational logo and button to enter app

--- app/src/main/java/org/traccar/client/MainActivity.java
+++ app/src/main/java/org/traccar/client/MainActivity.java
@@ -15,16 +15,57 @@
  */
 package org.traccar.client;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    //Intent intent = new Intent(MainActivity.this, MainApplication.class);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        setContentView(R.layout.activity_login);
+        //final EditText username = (EditText) findViewById(R.id.username);
+       // final EditText password = (EditText) findViewById(R.id.password);
+        final ImageView logo = (ImageView) findViewById(R.id.logo);
+        logo.startAnimation(AnimationUtils.loadAnimation(MainActivity.this,R.anim.side_slide));
+        String [] usernames={"one","two","three","four"};
+        final Button login = (Button) findViewById(R.id.loginButton);
+        login.setEnabled(true);
+
+        login.setOnClickListener(v -> setContentView(R.layout.main));
+
+
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void checkInput (String us, String pa){
+      /*  boolean isUser =false;
+        for (int i=0;i<usernames.length;i++){
+            if (usernames[i].equals(us));
+                isUser=true;*/
+
+
+
+        }
+
     }
 
-}
+
+
